8ed578c3c2ab5d91b566720f442a08d1a87ddf7c
Aditya Rai <adityarai2310@gmailcom>
2026-04-18 12:39:12 +0530

    fix cors backend

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,7 @@ const app = express();
 
 // ── Middleware ────────────────────────────────
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
-  credentials: true,
+  origin: "*"
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
