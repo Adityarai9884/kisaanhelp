4530b080e7dc91a3bf2679182d88c9e27c3aa62f
Aditya Rai <adityarai2310@gmailcom>
2026-04-18 14:38:42 +0530

    fix cors for production

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ const app = express();
 
 // ── Middleware ────────────────────────────────
 app.use(cors({
-  origin: ["http://localhost:3001"],
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "https://kisaanhelp.vercel.app"
+  ],
   credentials: true
 }));
 app.use(express.json());
