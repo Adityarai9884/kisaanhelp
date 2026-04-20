1a8d855c6446d75a8fd79bb5d4df2e7a59f6fb26
Aditya Rai <adityarai2310@gmailcom>
2026-04-20 12:16:38 +0530

    final route fix

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,7 @@ app.use(cors({
   credentials: true
 }));
 app.use(express.json());
+app.use("/api/auth", require("./routes/auth"));
 app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'development') {
